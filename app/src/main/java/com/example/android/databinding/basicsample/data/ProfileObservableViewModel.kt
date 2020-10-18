276c50c8579004449cde3d3650fce331c403247e
bjoernwe <bjoernwe@users.noreply.github.com>
2020-10-18 13:47:59 +0200

    Initial file import

---
+++ app/src/main/java/com/example/android/databinding/basicsample/data/ProfileObservableViewModel.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.data
+
+import android.os.CountDownTimer
+import androidx.databinding.ObservableInt
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * This class is used as a variable in the XML layout and it's fully observable, meaning that
+ * changes to any of the exposed observables automatically refresh the UI. *
+ */
+class ProfileLiveDataViewModel : ViewModel() {
+    private val _name = MutableLiveData("Ada")
+    private val _lastName = MutableLiveData("Lovelace")
+    private val _likes =  MutableLiveData(0)
+
+    val name: LiveData<String> = _name
+    val lastName: LiveData<String> = _lastName
+    val likes: LiveData<Int> = _likes
+
+    // popularity is exposed as LiveData using a Transformation instead of a @Bindable property.
+    val popularity: LiveData<Popularity> = Transformations.map(_likes) {
+        when {
+            it > 9 -> Popularity.STAR
+            it > 4 -> Popularity.POPULAR
+            else -> Popularity.NORMAL
+        }
+    }
+
+    fun onLike() {
+        _likes.value = (_likes.value ?: 0) + 1
+    }
+
+    fun startCountdown() {
+        val timerDuration: Long = 1000*(_likes.value ?: 0).toLong()
+        val timer = object: CountDownTimer(timerDuration, 1000) {
+            override fun onTick(millisRemaining: Long) {
+                _likes.value = (millisRemaining / 1000).toInt()
+            }
+
+            override fun onFinish() {
+                _likes.value = 0
+            }
+
+        }.start()
+    }
+}
+
+enum class Popularity {
+    NORMAL,
+    POPULAR,
+    STAR
+}
+
+private fun ObservableInt.increment() {
+    set(get() + 1)
+}
