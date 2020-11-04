0898054805daf92dd637c7be2c17c63f6f54cb5a
bjoernwe <bjoernwe@users.noreply.github.com>
2020-11-04 19:13:08 +0100

    Add binding adapters

---
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/BindingAdapters.kt
@@ -0,0 +1,14 @@
+package com.example.android.databinding.basicsample.ui
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+
+object BindingAdapters {
+
+    @BindingAdapter("visibleWhen")
+    @JvmStatic fun visibleWhen(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+}
