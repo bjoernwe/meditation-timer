ac00e0cf0ee25c8bb15dd6ff70d4d7e1dd501d59
bjoernwe <bjoernwe@users.noreply.github.com>
2020-11-05 16:28:30 +0100

    Add FINISHED state

--- app/src/main/java/com/example/android/databinding/basicsample/data/MeditationTimer.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/data/MeditationTimer.kt
@@ -6,7 +6,7 @@ import kotlin.math.round
 
 
 enum class TimerStates {
-    WAITING_FOR_START, RUNNING, WAITING_FOR_FEEDBACK
+    WAITING_FOR_START, RUNNING, FINISHED, WAITING_FOR_FEEDBACK
 }
 
 
@@ -30,6 +30,7 @@ class MeditationTimer(private var duration: Double = 10.0) {
 
             override fun onFinish() {
                 secondsLeft.value = 0
+                state.value = TimerStates.FINISHED  // trigger bell
                 state.value = TimerStates.WAITING_FOR_FEEDBACK
             }
 
