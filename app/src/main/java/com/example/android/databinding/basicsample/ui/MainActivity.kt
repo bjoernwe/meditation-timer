0cf703822543fdbf154106695bf01def230df981
bjoernwe <bjoernwe@users.noreply.github.com>
2020-11-05 23:24:46 +0100

    Prepare media player asynchronously

--- app/src/main/java/com/example/android/databinding/basicsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.databinding.basicsample.ui
 
 import android.media.MediaPlayer
@@ -27,9 +11,10 @@ import com.example.android.databinding.basicsample.R
 import com.example.android.databinding.basicsample.data.TimerStates
 import com.example.android.databinding.basicsample.data.TimerViewModel
 import com.example.android.databinding.basicsample.databinding.MainActivityBinding
+import timber.log.Timber
 
 
-class MainActivity : AppCompatActivity(), Observer<TimerStates>, RatingBar.OnRatingBarChangeListener {
+class MainActivity : AppCompatActivity(), Observer<TimerStates>, RatingBar.OnRatingBarChangeListener, MediaPlayer.OnErrorListener {
 
     var mediaPlayer: MediaPlayer? = null
 
@@ -48,12 +33,17 @@ class MainActivity : AppCompatActivity(), Observer<TimerStates>, RatingBar.OnRat
         // Register event callbacks
         timerViewModel.state.observe(this, this)
         binding.ratingBar.onRatingBarChangeListener = this
+        mediaPlayer?.setOnErrorListener(this)
 
     }
 
     override fun onStart() {
         super.onStart()
-        mediaPlayer = MediaPlayer.create(this, R.raw.bell_347378)
+        applicationContext.resources.openRawResourceFd(R.raw.bell_347378).use {
+            mediaPlayer = MediaPlayer()
+            mediaPlayer?.setDataSource(it)
+            mediaPlayer?.prepareAsync()
+        }
     }
 
     override fun onStop() {
@@ -80,6 +70,11 @@ class MainActivity : AppCompatActivity(), Observer<TimerStates>, RatingBar.OnRat
             timerViewModel.submitRating(rating, ratingBar.max)
             ratingBar.rating = 0F
         }
+    }
+
+    override fun onError(mediaPlayer: MediaPlayer?, what: Int, extra: Int): Boolean {
+        Timber.e("MediaPlayer Error $what")
+        return false
     }
 
 }
