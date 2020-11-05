41d7308d6fc5e3894c46dd7a56aa856eabd9f19a
Björn <bjoernwe@users.noreply.github.com>
2020-11-05 20:32:58 +0100

    Add Meditation Bell (#4)
    * Add FINISHED state
    
    * Add Timber logging
    
    * Play bell

--- app/src/main/java/com/example/android/databinding/basicsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/MainActivity.kt
@@ -16,37 +16,69 @@
 
 package com.example.android.databinding.basicsample.ui
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.RatingBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.databinding.basicsample.R
+import com.example.android.databinding.basicsample.data.TimerStates
 import com.example.android.databinding.basicsample.data.TimerViewModel
 import com.example.android.databinding.basicsample.databinding.MainActivityBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Observer<TimerStates>, RatingBar.OnRatingBarChangeListener {
+
+    var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
         // Obtain ViewModel from ViewModelProviders
-        val viewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
+        val timerViewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
 
         // Obtain binding
         val binding: MainActivityBinding = DataBindingUtil.setContentView(this, R.layout.main_activity)
-        binding.viewmodel = viewModel
+        binding.viewmodel = timerViewModel
         binding.lifecycleOwner = this
 
-        // RatingBar change callback
-        binding.ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
-            if (fromUser) {
-                viewModel.submitRating(rating, ratingBar.max)
-                ratingBar.rating = 0F
-            }
-        }
+        // Register event callbacks
+        timerViewModel.state.observe(this, this)
+        binding.ratingBar.onRatingBarChangeListener = this
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mediaPlayer = MediaPlayer.create(this, R.raw.bell_347378)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    override fun onChanged(newTimerState: TimerStates?) {
+        when (newTimerState) {
+            TimerStates.FINISHED -> { playBell() }
+            else -> {}
+        }
+    }
+
+    private fun playBell() {
+        mediaPlayer?.start()
+    }
+
+    override fun onRatingChanged(ratingBar: RatingBar, rating: Float, fromUser: Boolean) {
+        if (fromUser) {
+            val timerViewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
+            timerViewModel.submitRating(rating, ratingBar.max)
+            ratingBar.rating = 0F
+        }
     }
 
 }
