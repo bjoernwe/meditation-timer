22de0bebdd4a33a87716bac40248d6510103becd
bjoernwe <bjoernwe@users.noreply.github.com>
2020-11-06 12:16:50 +0100

    Make mutable members private

--- app/src/main/java/com/example/android/databinding/basicsample/data/MeditationTimer.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/data/MeditationTimer.kt
@@ -1,6 +1,7 @@
 package com.example.android.databinding.basicsample.data
 
 import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlin.math.round
 
@@ -12,26 +13,29 @@ enum class TimerStates {
 
 class MeditationTimer(private var duration: Double = 10.0) {
 
-    val state = MutableLiveData(TimerStates.WAITING_FOR_START)
-    val secondsLeft =  MutableLiveData(0)
+    private val _state = MutableLiveData(TimerStates.WAITING_FOR_START)
+    private val _secondsLeft =  MutableLiveData(0)
+
+    val state: LiveData<TimerStates> = _state
+    val secondsLeft: LiveData<Int> = _secondsLeft
 
     fun startCountdown() {
 
-        secondsLeft.value = duration.toInt()
-        state.value = TimerStates.RUNNING
+        _secondsLeft.value = duration.toInt()
+        _state.value = TimerStates.RUNNING
 
         val timerDuration: Long = (secondsLeft.value ?: 0).toLong() * 1000
 
         object: CountDownTimer(timerDuration, 1000) {
 
             override fun onTick(millisRemaining: Long) {
-                secondsLeft.value = round(millisRemaining / 1000.0).toInt()
+                _secondsLeft.value = round(millisRemaining / 1000.0).toInt()
             }
 
             override fun onFinish() {
-                secondsLeft.value = 0
-                state.value = TimerStates.FINISHED  // trigger bell
-                state.value = TimerStates.WAITING_FOR_FEEDBACK
+                _secondsLeft.value = 0
+                _state.value = TimerStates.FINISHED  // trigger bell
+                _state.value = TimerStates.WAITING_FOR_FEEDBACK
             }
 
         }.start()
@@ -39,7 +43,7 @@ class MeditationTimer(private var duration: Double = 10.0) {
     }
 
     fun submitRating(rating: Float) {
-        state.value = TimerStates.WAITING_FOR_START
+        _state.value = TimerStates.WAITING_FOR_START
         duration *= if (rating >= .5) 1.1 else 0.8
     }
 
