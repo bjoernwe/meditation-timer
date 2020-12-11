cb73e269d61a1069d73b15d2c7244a1ccdcbf112
Björn <bjoernwe@users.noreply.github.com>
2020-12-11 17:27:47 +0100

    Ripple Effect (#20)
    * Parameterize circle
    
    * Update configuration
    
    * Check state before starting timer
    
    * Extend ViewModel
    
    * Remove ActionBar
    
    * Remove unused file
    
    * Switch UI to Jetpack Compose
    
    * Switch UI to Jetpack Compose
    
    * chore: Update dependencies
    
    * chore: Update dependencies
    
    * chore: Update dependencies
    
    * Add modifier to text
    
    * Add ripple effect
    
    * Disable ripple when running
    
    * Format code

--- app/src/main/java/app/upaya/timer/ui/MainActivity.kt
+++ app/src/main/java/app/upaya/timer/ui/MainActivity.kt
@@ -34,8 +34,8 @@ class MainActivity : AppCompatActivity(), MediaPlayer.OnErrorListener {
         // Emit Main Composable
         setContent {
             MainComposable(
-                    isRunning = timerViewModel.isRunning.observeAsState(),
-                    secondsLeft = timerViewModel.secondsLeftString.observeAsState(),
+                    isRunning = timerViewModel.isRunning.observeAsState(initial = false),
+                    secondsLeft = timerViewModel.secondsLeftString.observeAsState(initial = ""),
                     onClick = { onCircleClicked(timerViewModel) }
             )
         }
