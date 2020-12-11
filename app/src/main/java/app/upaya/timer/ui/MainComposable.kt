363c2bb1c5d8144fbc16434d9fa67701b2678ca9
bjoernwe <bjoernwe@users.noreply.github.com>
2020-12-11 16:50:31 +0100

    Add ripple effect

--- app/src/main/java/app/upaya/timer/ui/MainComposable.kt
+++ app/src/main/java/app/upaya/timer/ui/MainComposable.kt
@@ -2,13 +2,26 @@ package app.upaya.timer.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.ui.tooling.preview.Preview
 
 
 @Composable
-fun MainComposable(isRunning: State<Boolean?>, secondsLeft: State<String?>, onClick: () -> Unit) {
+fun MainComposable(isRunning: State<Boolean>, secondsLeft: State<String>, onClick: () -> Unit) {
 
     TimerTheme {
         TimerRing(isRunning = isRunning, secondsLeft = secondsLeft, onClick = onClick)
     }
 
+}
+
+
+@Preview
+@Composable
+fun MainComposablePreview() {
+    MainComposable(
+            isRunning = mutableStateOf(false),
+            secondsLeft = mutableStateOf("0:00:00"),
+            onClick = { }
+    )
 }
--- app/src/main/java/app/upaya/timer/ui/TimerRing.kt
+++ app/src/main/java/app/upaya/timer/ui/TimerRing.kt
@@ -1,12 +1,11 @@
 package app.upaya.timer.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
+import androidx.compose.material.ripple.rememberRippleIndication
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,26 +13,36 @@ import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun TimerRing(isRunning: State<Boolean?>, secondsLeft: State<String?>, onClick: () -> Unit) {
+fun TimerRing(isRunning: State<Boolean>, secondsLeft: State<String>, onClick: () -> Unit) {
+
+    // Ripple effect when clicking ring
+    val rippleIndication = rememberRippleIndication(
+            radius = 0.dp,
+            bounded = false,
+            color = Color.Transparent
+    )
 
     Box(contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .clickable(onClick = onClick)
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)
+            modifier = Modifier
+                    .background(MaterialTheme.colors.background)
+                    .fillMaxSize()
+                    .clickable(
+                            onClick = onClick,
+                            indication = rippleIndication,
+                    )
     ) {
 
         // Ring
         MaterialRing(
-                size = 200.dp,
-                thickness = 16.dp,
-                depth = 3.dp,
-                color = if (isRunning.value == true) Color.White else teal200
+            size = 200.dp,
+            thickness = 16.dp,
+            depth = 3.dp,
+            color = if (isRunning.value) Color.White else teal200
         )
 
         // Text
-        if (isRunning.value == true) {
-            secondsLeft.value?.let { ShadowText(text = it) }
+        if (isRunning.value) {
+            ShadowText(text = secondsLeft.value)
         }
 
     }
