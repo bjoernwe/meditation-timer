1e76bad18ac59f4c60382c4963ddd30dc9583934
bjoernwe <bjoernwe@users.noreply.github.com>
2020-12-28 17:14:20 +0100

    Update preferences

--- app/src/main/java/app/upaya/timer/timer/TimerViewModelFactory.kt
+++ app/src/main/java/app/upaya/timer/timer/TimerViewModelFactory.kt
@@ -1,19 +1,19 @@
 package app.upaya.timer.timer
 
+import android.app.Application
 import android.content.Context
-import android.preference.PreferenceManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import app.upaya.timer.R
 import java.lang.IllegalArgumentException
 
 /* This factory allows passing arguments to the TimerViewModel*/
-class TimerViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+class TimerViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TimerViewModel::class.java)) {
-            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-            val initialSessionLength = prefs.getFloat(context.getString(R.string.pref_session_length), 10.0F).toDouble()
+            val prefs = application.getSharedPreferences(application.getString(R.string.preference_file), Context.MODE_PRIVATE)
+            val initialSessionLength = prefs.getFloat(application.getString(R.string.pref_session_length), 10.0F).toDouble()
             @Suppress("UNCHECKED_CAST")
             return TimerViewModel(initialSessionLength) as T
         } else {
