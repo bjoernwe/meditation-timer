2468f2eae6b75312062fbf4471b9e1055de3bfab
bjoernwe <bjoernwe@users.noreply.github.com>
2021-01-20 18:26:05 +0100

    Implement fake with transformations

--- app/src/androidTest/java/app/upaya/timer/sessions/SessionRepositoryFake.kt
+++ app/src/androidTest/java/app/upaya/timer/sessions/SessionRepositoryFake.kt
@@ -5,14 +5,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 
 
-class SessionRepositoryFake {
+class SessionRepositoryFake : ISessionRepository {
 
     private val _sessions = MutableLiveData<MutableList<Session>>(ArrayList())
 
-    val sessionCount: LiveData<Int> = Transformations.map(_sessions) { it.size }
-    val sessionAvg = Transformations.map(_sessions) { it.map { s -> s.length }.average().toFloat() }
+    override val sessionCount: LiveData<Int> = Transformations.map(_sessions) { it.size }
+    override val sessionAvg = Transformations.map(_sessions) {
+        val avg = it.map { s -> s.length }.average().toFloat()
+        if (avg.isNaN()) return@map 0f else return@map avg
+    }
 
-    suspend fun storeSession(session: Session) {
+    override suspend fun storeSession(session: Session) {
         _sessions.value!!.add(session)
     }
 
