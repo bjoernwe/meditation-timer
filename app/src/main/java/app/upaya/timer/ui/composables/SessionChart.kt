ef842e60e29c4b2876888520466d9374e028b5ab
bjoernwe <bjoernwe@users.noreply.github.com>
2021-01-25 18:44:13 +0100

    Make SessionChart state-less

--- app/src/main/java/app/upaya/timer/ui/composables/SessionChart.kt
+++ app/src/main/java/app/upaya/timer/ui/composables/SessionChart.kt
@@ -2,22 +2,18 @@ package app.upaya.timer.ui.composables
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.viewinterop.viewModel
-import app.upaya.timer.sessions.SessionViewModel
+import app.upaya.timer.sessions.SessionAggregate
 import com.db.williamchart.view.BarChartView
 
 
 @Composable
-fun SessionChart(modifier: Modifier = Modifier) {
+fun SessionChart(sessionAggregates: State<List<SessionAggregate>>, modifier: Modifier = Modifier) {
 
-    val sessionViewModel: SessionViewModel = viewModel()
-    val sessionAggregates = sessionViewModel.sessionAggOfLastDays.observeAsState()
-
-    val sessionEntries = sessionAggregates.value?.reversed()?.mapIndexed { index, aggregate ->
+    val sessionEntries = sessionAggregates.value.reversed().mapIndexed { index, aggregate ->
         index.toString() to aggregate.sessionCount.toFloat()
     }
 
@@ -34,9 +30,7 @@ fun SessionChart(modifier: Modifier = Modifier) {
                 }
             }
     ) {
-        if (sessionEntries != null) {
-            it.show(sessionEntries)
-        }
+        it.show(sessionEntries)
     }
 
 }
