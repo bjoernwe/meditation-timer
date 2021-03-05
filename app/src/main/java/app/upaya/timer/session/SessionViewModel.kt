4e7cddcbbaa2625cbf891c84ecf915848eaa6bd1
bjoernwe <bjoernwe@users.noreply.github.com>
2021-03-05 19:17:10 +0100

    chore: Clean-up view model

--- app/src/main/java/app/upaya/timer/session/SessionViewModel.kt
+++ app/src/main/java/app/upaya/timer/session/SessionViewModel.kt
@@ -19,34 +19,4 @@ class SessionViewModel(sessionHandler: SessionHandler) : ViewModel() {
     val isRunning: LiveData<Boolean> = Transformations.map(state) { it is Running }
     val isIdle: LiveData<Boolean> = Transformations.map(state) { it is Idle }
 
-    // Event Handling
-    // We use observeForever() because we don't want to have any LivecycleOwner in the ViewModel.
-    // The observer is removed in onCleared().
-    private var stateObserver: Observer<SessionState> = Observer(::onTimerStateChanged)
-    init { state.observeForever(stateObserver) }
-
-    /**
-     * Timer events / callbacks
-     */
-
-    private fun onTimerStateChanged(newState: SessionState) {
-        when (newState) {
-            is Idle -> {}
-            is Running -> {}
-            is Finished -> { storeFinishedSession() }
-        }
-    }
-
-    private fun storeFinishedSession() {
-        //sessionLength.value?.let {
-        //    MainScope().launch { sessionHistoryRepository.storeSession(it) }
-        //}
-    }
-
-    // ViewModel destructor
-    override fun onCleared() {
-        super.onCleared()
-        state.removeObserver(stateObserver)
-    }
-
 }
