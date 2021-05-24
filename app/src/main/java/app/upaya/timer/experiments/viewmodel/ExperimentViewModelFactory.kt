df30ce91d688b00b54fe2a60384dd19cee606de4
bjoernwe <bjoernwe@users.noreply.github.com>
2021-05-24 15:24:21 +0200

    refactor: Rename ExperimentCreator

--- app/src/main/java/app/upaya/timer/experiments/viewmodel/ExperimentViewModelFactory.kt
+++ app/src/main/java/app/upaya/timer/experiments/viewmodel/ExperimentViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import app.upaya.timer.MeditationTimerApplication
 import app.upaya.timer.experiments.probes.ProbeRepository
-import app.upaya.timer.experiments.creator.ExperimentCreator
+import app.upaya.timer.experiments.creator.ExperimentCreatorGlobalLength
 import app.upaya.timer.experiments.repositories.logs.ExperimentLogRepository
 import app.upaya.timer.experiments.repositories.logs.room.ExperimentLogDatabase
 import app.upaya.timer.experiments.repositories.length.ExperimentLengthRepository
@@ -41,7 +41,7 @@ class ExperimentViewModelFactory(private val context: Context) : ViewModelProvid
             val experimentLengthRepository = ExperimentLengthRepository(context)
 
             // ExperimentCreator
-            val experimentCreator = ExperimentCreator(
+            val experimentCreator = ExperimentCreatorGlobalLength(
                 probeRepository = probeRepository,
                 experimentLengthRepository = experimentLengthRepository,
             )
